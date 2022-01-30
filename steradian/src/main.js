ade8c81cbf414ea8b6b9da1c4dda6bbec310013d
Jeremy Rutledge <jerrutledge@gmail.com>
2022-01-30 11:04:35 -0400

    add gui and description

--- steradian/src/main.js
+++ steradian/src/main.js
@@ -1,8 +1,18 @@
 import * as THREE from './three.module.js';
 
 import {
+    GUI
+} from './lil-gui.module.min.js';
+
+import {
     OrbitControls
 } from "./OrbitControls.js";
+
+// these are the parameters changed by the gui controls
+const params = {
+    h: 1-(1/(2*Math.PI)), // start off by default at one steradian
+    // should be about 0.840845
+};
 
 // CREATE SCENE
 var scene = new THREE.Scene();
@@ -12,7 +22,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.localClippingEnabled = true;
 var canvasDimension = Math.min(900, window.innerWidth-90, window.innerHeight-90);
 renderer.setSize(canvasDimension, canvasDimension);
-document.body.appendChild(renderer.domElement);
+document.getElementById("demo").appendChild(renderer.domElement);
 
 // SPHERE
 var geometry = new THREE.SphereGeometry(1, 32, 16);
@@ -25,22 +35,20 @@ scene.add(sphere);
 
 // CONE
 // this h represents the height of the cone, not the height of the cap
-var h = 1-(1/(2*Math.PI)); // should be about 0.840845
-var r = (1-h ** 2) ** (1/2); // height of the cap = 0.1591549
-var geometry = new THREE.ConeGeometry(r, h, 64, 1, true);
-var material = new THREE.MeshPhongMaterial({
+var geometry = new THREE.ConeGeometry(1, 1, 64, 1, true); // automatically consults h param
+const coneMaterial = new THREE.MeshPhongMaterial({
     color: 0xD254F0
 });
-var cone = new THREE.Mesh(geometry, material);
-cone.lookAt(0, 5, 0);
-cone.position.z = (h/2);
+const cone = new THREE.Mesh(geometry, coneMaterial);
+cone.lookAt(0, 5, 0); // rotate 90
+updateConeShape();
 scene.add(cone);
 
 // CONE CAP
 const clipPlanes = [
     new THREE.Plane(new THREE.Vector3(1, 0, 0), 3),
     new THREE.Plane(new THREE.Vector3(0, -1, 0), 3),
-    new THREE.Plane(new THREE.Vector3(0, 0, 1), -h)
+    new THREE.Plane(new THREE.Vector3(0, 0, 1), -params.h)
 ];
 var geometry = new THREE.SphereGeometry(1.001, 64, 32); // 1.001 to prevent clipping between cone and cap
 var material = new THREE.MeshPhongMaterial({
@@ -71,5 +79,27 @@ var animate = function () {
 
     renderer.render(scene, camera);
 };
+
+// GUI CONTROLS
+const gui = new GUI();
+
+gui.add(params, 'h', -1, 1).name('cone height (h)').onChange(function (value) {
+    // make a new cone and replace it
+    updateConeShape();
+    clipPlanes[2].constant = -params.h;
+});
+
+function updateConeShape() {
+    cone.geometry.dispose();
+    var r = (1-params.h ** 2) ** (1/2); // pythagorean theorum
+    cone.geometry = new THREE.ConeGeometry(r, params.h, 64, 1, true);
+    cone.position.z = (params.h/2); // move point to center
+
+    // also update the params in the html
+    document.getElementById("steradians").innerHTML = (Math.PI * 2 * (1-params.h)).toFixed(5);
+    document.getElementById("polarAngle").innerHTML = (Math.asin(r)).toFixed(5);
+    document.getElementById("coneHeight").innerHTML = (params.h).toFixed(5);
+    document.getElementById("capArea").innerHTML = (Math.PI * 2 * (1-params.h)).toFixed(5);
+}
 
 animate();
